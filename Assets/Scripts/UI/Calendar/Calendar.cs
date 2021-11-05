b87bbb6645f5d890f94e1542345af2243570cd0d
alexSolohin <solohinalex1@gmail.com>
2021-11-05 13:37:45 +0300

    add new bar UI

--- Assets/Scripts/UI/Calendar/Calendar.cs
+++ Assets/Scripts/UI/Calendar/Calendar.cs
@@ -1,34 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Calendar : MonoBehaviour
-{
-    [SerializeField] private Transform content;
-    [SerializeField] private GameObject monthPanelPref;
-    
-    private void Start()
-    {
-        StartCoroutine(CreateCalendar());
-    }
-
-    private IEnumerator CreateCalendar()
-    {
-        var yearCounter = DateTime.Today.Year;
-        var monthCounter = DateTime.Today.Month - 1;
-        for (int i = 0; i < 10; i++)
-        {
-            var montPanel = Instantiate(monthPanelPref, content).GetComponent<MonthPanel>();
-            var dateTime = new DateTime(yearCounter, monthCounter, 1);
-            monthCounter++;
-            if (monthCounter > 12)
-            {
-                monthCounter = 1;
-                yearCounter++;
-            }
-            yield return null;
-            montPanel.CreateMonth(dateTime);
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1aa2d01aeb2130d895d523a3ac08b0a01e748d1716f62e79ad5dd6c5645a2a5
+size 931
